7ee9c7c91b24dfcab53114ddf3ca9627c8fbf9be
oconner9 <oconner9@tcd.ie>
2023-03-02 16:40:18 +0000

    Second encryption added

--- MERCURY_Client.py
+++ MERCURY_Client.py
@@ -16,6 +16,24 @@ def RSAEncrypt(data, recipient_key):
     [ file_out.write(x) for x in (enc_session_key, cipher_aes.nonce, tag, ciphertext) ]
     file_out.close()
 
+def SecondEncrypt(message):
+    key = int(input('what is the numeric key?\n'))
+
+    words = message.split(' ')
+    new_string = []
+    temp= ''
+    for i in words:
+        for j in i:
+        
+            num = ord(j) + key
+            char = chr(num)   
+            temp = temp + char
+        temp = temp + ' ' # see if we wanna keep this 
+    new_string.append(temp[::-1]) 
+
+    reverse = ''.join(reversed(new_string))
+    return(reverse)
+
 # Main
 serverName = 'localhost'
 serverPort = 12000
@@ -27,6 +45,8 @@ sentence = input('Input message to be encrypted and sent to the server:')
 while len(sentence) > 1000:
     print("Message is too long. Please try again.")
     sentence = input('Input message to be encrypted and sent to the server:')
+
+sentence = SecondEncrypt(sentence)
 
 sentence = sentence.encode("utf-8")
 file_out = open("encrypted_data.bin", "wb")
